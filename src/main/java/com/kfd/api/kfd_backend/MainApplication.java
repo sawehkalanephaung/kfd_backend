095d93d96664d2ea225e6996557a8c6e0d2f1fe3
Eh Soe <sawehsoe996@gmail.com>
2026-06-03 16:45:57 +0700

    first commit

---
+++ src/main/java/com/kfd/api/kfd_backend/MainApplication.java
@@ -0,0 +1,13 @@
+package com.kfd.api.kfd_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MainApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kfd/api/kfd_backend/KfdBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kfd.api.kfd_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KfdBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
