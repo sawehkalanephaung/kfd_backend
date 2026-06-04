1083403d4a461c4805e9d997d3749d0f183893d7
Eh Soe <sawehsoe996@gmail.com>
2026-06-05 06:24:51 +0700

    feat: add TeamMemberDto and update FaqDto with lombok boilerplate annotations

--- src/main/java/com/kfd/api/kfd_backend/faq/FaqDto.java
+++ src/main/java/com/kfd/api/kfd_backend/faq/FaqDto.java
@@ -1,11 +1,16 @@
 package com.kfd.api.kfd_backend.faq;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.util.UUID;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class FaqDto {
     private UUID id;
     private String question;
