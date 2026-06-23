7c9e07b39636f97e367011c2d03260728d2dda40
Eh Soe <sawehsoe996@gmail.com>
2026-06-23 10:31:20 +0700

    chore: remove dynamic footer link entities and apis

--- src/main/java/com/kfd/api/kfd_backend/settings/footer/FooterLinkRepository.java
+++
@@ -1,8 +0,0 @@
-package com.kfd.api.kfd_backend.settings.footer;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.UUID;
-
-public interface FooterLinkRepository extends JpaRepository<FooterLink, UUID> {
-}
---
+++ src/main/resources/db/migration/V18__drop_footer_link_tables.sql
@@ -0,0 +1,6 @@
+-- ============================================================
+-- V18: Drop Footer Links Tables
+-- ============================================================
+
+DROP TABLE IF EXISTS footer_links CASCADE;
+DROP TABLE IF EXISTS footer_link_sections CASCADE;
