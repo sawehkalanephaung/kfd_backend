83509496e3cd89c5058ca01bfea754432e63fb1d
Eh Soe <sawehsoe996@gmail.com>
2026-08-11 00:41:33 +0700

    Fix Flyway eager initialization and add missing starter

--- src/main/java/com/kfd/api/kfd_backend/auth/ApplicationConfig.java
+++ src/main/java/com/kfd/api/kfd_backend/auth/ApplicationConfig.java
@@ -4,6 +4,7 @@ import com.kfd.api.kfd_backend.user.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
@@ -14,16 +15,20 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
-@RequiredArgsConstructor
 public class ApplicationConfig {
 
     private final UserRepository userRepository;
+
+    public ApplicationConfig(@Lazy UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
 
     @Bean
     public UserDetailsService userDetailsService() {
         return username -> userRepository.findByEmail(username)
                 .orElseThrow(() -> new UsernameNotFoundException("User not found"));
     }
+
 
     @Bean
     public AuthenticationProvider authenticationProvider() {
